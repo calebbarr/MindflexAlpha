3f9fd6e3378768781038bd11ea9d61949f72416d
Caleb Barr <caleb@xbarr.me>
2015-05-15 14:20:18 -0700

    fixed python test app

--- extra/mindflex_test.py
+++ extra/mindflex_test.py
@@ -5,15 +5,19 @@ from twisted.internet import reactor
 from twisted.internet import task
 
 
-clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-clientsocket.connect(('localhost', 9999))
 def sendFakeBrainwaves():
-    clientsocket.send(",".join(
+    brainwaves = ",".join(
     [str(x) for x in \
         [0] +\
         [r(0,100) for x in range(2)] +\
         [r(0,100000) for x in range(8)]
-    ])+"\n")
+    ])+"\n"
+    print brainwaves
+    clientsocket.send(brainwaves)
 
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('localhost', 9999))
+serversocket.listen(1)
+clientsocket, address = serversocket.accept()
 task.LoopingCall(sendFakeBrainwaves).start(1.0)
 reactor.run()
